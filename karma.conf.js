95b13be722be5f028604e2935a6bbb3422e2f42f
Markus Kobler <markus@distinctive.co>
2014-03-20 12:25:48 +0000

    Partial support for sourcelabs crossbrowser testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
-module.exports = function(config) {
+module.exports = function(config, options) {
   config.set({
-    basePath: '',
     frameworks: ['mocha','chai'],
     files: [
       'dist/**/*.js',
@@ -12,8 +11,66 @@ module.exports = function(config) {
     autoWatch: true,
     captureTimeout: 60000,
     singleRun: false,
+    browsers: ['Chrome'], // 'Firefox', 'PhantomJS'
     
-    browsers: ['Chrome'] // 'Firefox', 'PhantomJS'
+    sauceLabs: {
+      testName: 'cohesive.js',
+      startConnect: true
+    },
+
+    customLaunchers: {
+      'SL_Chrome': {
+        base: 'SauceLabs',
+        browserName: 'chrome'
+      },
+      'SL_Firefox': {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        version: '26'
+      },
+      'SL_Safari': {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.9',
+        version: '7'
+      },
+      'SL_IE_7': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows XP',
+        version: '7'
+      },
+      'SL_IE_8': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows XP',
+        version: '8'
+      },
+      'SL_IE_9': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '9'
+      },
+      'SL_IE_10': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2012',
+        version: '10'
+      },
+      'SL_IE_11': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      }
+    }
   })
 
+  if (process.env.TRAVIS) {
+    config.sauceLabs.build = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+    config.sauceLabs.startConnect = false;
+    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    config.transports = ['xhr-polling'];
+  }
 }
